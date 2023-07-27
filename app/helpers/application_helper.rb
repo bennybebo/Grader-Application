c0a0d9c97dff2e107ac8f1d5b03953e6b8ba1f51
bennybebo <burstein.20@buckeyemail.osu.edu>
2023-07-27 16:18:38 -0400

    Added meeting information to sections page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    def meeting_day_string(meeting)
+        days = []
+        days << "Monday" if meeting.monday
+        days << "Tuesday" if meeting.tuesday
+        days << "Wednesday" if meeting.wednesday
+        days << "Thursday" if meeting.thursday
+        days << "Friday" if meeting.friday
+        days << "Saturday" if meeting.saturday
+        days << "Sunday" if meeting.sunday
+        days.join(", ")
+      end
+
+      def formatted_time_range(start_time, end_time)
+        "#{format_time(start_time)} - #{format_time(end_time)}"
+      end
+    
+      def format_time(time)
+        time.strftime('%I:%M %p')
+      end
 end
