3732d5033b8993e5fc0edc7110c28a4acea1ee5e
Lang Min <min.276@osu.edu>
2023-07-28 14:03:34 -0400

    Remove confusing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,3 @@
 #   Character.create(name: "Luke", movie: movies.first)
 user = User.new(email: 'admin.1@osu.edu', password: 'password', user_type: 'Admin',  approved: true)
 user.save!
-
-course = Course.new(course_number: 'CSE 3901',course_name: 'Project: Web Apps', course_description: 'It is a nice course.', 
-    campus: 'COL', term: 'Summer')
-course.save!
-
-section = Section.new(section_number: '6442', class_number: '40', course_number: 'CSE 3901', component: 'CSE', 
-    graders_needed: '1', graders_assigned: '1')
-section.save!
-
-meeting = Meeting.new(section_number: '6442', class_number: '40', start_time: Time.parse("6:30 PM"), 
-    end_time: Time.parse("8:00 PM"), monday: false, tuesday: true, wednesday: false, thursday: true, friday: false, 
-    location: 'online' )
-meeting.save!
